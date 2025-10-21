2d80c4adb8e875b99f506b48d1a028194e989e53
Alex Lima <alexoliveira.lima111@gmail.com>
2025-10-21 14:48:59 -0300

    Fix paths and formatting in los.js

--- Athon/los.js
+++ Athon/los.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const PATHS = { carne: 'carne.html', off: '/desh_inicio/off.html' };
+    const PATHS = { carne: 'carne.html', off: '/off.html' };
 
     const floatingBtn = document.getElementById('floating-btn');
     const menuPanel = document.getElementById('menu-panel');
@@ -88,3 +88,4 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 
 });
+
